f1f3e5b3b743f8cf38b039b35415425811c411b0
Paula Añez Urquizo <paula.urquizo@gmail.com>
2023-05-08 22:55:54 +0200

    challenge1 - password input field validation finished

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,38 @@
-import { hello } from './index';
+import { MESSAGE_ERROR_MINOR_LONG, MESSAGE_ERROR_NOT_CONTAIN_NUMBER, MESSAGE_ERROR_ONE_CAPITAL_LETTER, MESSAGE_ERROR_ONE_SPECIAL_CHARACTER, passwordValidation } from './index';
 
-describe('Testing jest config', () => {
-  test('Const must have hello value', () => {
-    expect(hello).toBe('HELLO CONFIGS');
+describe('Password validation', () => {
+  test('When password is undefined then it returns a falsy value', () => {
+    expect(passwordValidation(undefined as unknown as string).isValidate).toBeFalsy();
+  });
+  describe('When password is minor than 8 and not contain at least 2 numbers', () => {
+    test('When password is minor than 8 then it returns a falsy value and a message', () => {
+      const { message, isValidate } = passwordValidation('lre34T$');
+      expect(message).toBe(MESSAGE_ERROR_MINOR_LONG);
+      expect(isValidate).toBeFalsy();
+    });
+    test('When password must not contain at least 2 numbers then it returns a falsy value and an error message', () => {
+      const { message, isValidate } = passwordValidation('lerelararaTB%');
+      expect(message).toBe(MESSAGE_ERROR_NOT_CONTAIN_NUMBER);
+      expect(isValidate).toBeFalsy();
+    });
+    test('When password must not be minor than 8 and contain at least 2 numbers then it returns a falsy value and an error message', () => {
+      const { message, isValidate } = passwordValidation('lere@B');
+      const twoErrorMessage = MESSAGE_ERROR_MINOR_LONG.concat('\n', MESSAGE_ERROR_NOT_CONTAIN_NUMBER);
+      expect(message).toBe(twoErrorMessage);
+      expect(isValidate).toBeFalsy();
+    });
+  });
+
+  describe('When password not contain at least one capital letter and one special character', () => {
+    test('When password must not contain at least one capital letter it returns a falsy value and a error message', () => {
+      const { message, isValidate } = passwordValidation('lerelarara$123');
+      expect(message).toBe(MESSAGE_ERROR_ONE_CAPITAL_LETTER);
+      expect(isValidate).toBeFalsy();
+    });
+    test('When password must not contain at least one special character it returns a falsy value and a error message', () => {
+      const { message, isValidate } = passwordValidation('lerelaB723456');
+      expect(message).toBe(MESSAGE_ERROR_ONE_SPECIAL_CHARACTER);
+      expect(isValidate).toBeFalsy();
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,44 @@
-export const hello = 'HELLO CONFIGS';
-export const bye = 'BYE';
+export const MESSAGE_ERROR_MINOR_LONG = 'Password must be at least 8 characters';
+export const MESSAGE_ERROR_NOT_CONTAIN_NUMBER = 'The password must contain at least 2 numbers';
+export const MESSAGE_ERROR_ONE_CAPITAL_LETTER = 'Password must contain at least one capital letter';
+export const MESSAGE_ERROR_ONE_SPECIAL_CHARACTER = 'Password must contain at least one special character';
+
+interface Validation {
+  isValidate: boolean;
+  message?: string;
+}
+
+const regexMiniumTwoNumbers = /(?=\d{2,})/g;
+const regexOneCapitalLetter = /[A-Z]+/;
+const regexSpecialCharacter = /(?=.*[@#$%^&+=]).*$/;
+const validatorMajorToEightLong = (password: string): boolean => password.length >= 8 && true;
+const validatorWith2Numbers = (password: string): boolean => regexMiniumTwoNumbers.test(password) && true;
+const validatorOneCapitalLetter = (password: string): boolean => regexOneCapitalLetter.test(password) && true;
+const validatorSpecialCharacter = (password: string): boolean => regexSpecialCharacter.test(password) && true;
+
+const errorMessage = (validationMessage: string | undefined, messageError: string) => validationMessage === undefined ? messageError : validationMessage.concat('\n', messageError);
+
+export const passwordValidation = (password: string): Validation => {
+  let validationOfPassword:Validation = { isValidate: true };
+  if (password === undefined) {
+    return { isValidate: false };
+  }
+
+  if (!validatorMajorToEightLong(password)) {
+    validationOfPassword = { isValidate: false, message: MESSAGE_ERROR_MINOR_LONG };
+  }
+
+  if (!validatorWith2Numbers(password)) {
+    validationOfPassword = { isValidate: false, message: errorMessage(validationOfPassword?.message, MESSAGE_ERROR_NOT_CONTAIN_NUMBER) };
+  }
+
+  if (!validatorOneCapitalLetter(password)) {
+    validationOfPassword = { isValidate: false, message: errorMessage(validationOfPassword?.message, MESSAGE_ERROR_ONE_CAPITAL_LETTER) };
+  }
+
+  if (!validatorSpecialCharacter(password)) {
+    validationOfPassword = { isValidate: false, message: errorMessage(validationOfPassword?.message, MESSAGE_ERROR_ONE_SPECIAL_CHARACTER) };
+  }
+
+  return validationOfPassword;
+};
